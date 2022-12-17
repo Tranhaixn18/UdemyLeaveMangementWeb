061319b941841eeeaaace1743ee3d53862f1bffc
Tranhaixn18 <haitran17042k@gmail.com>
2022-12-17 21:16:50 +0700

    commit session5

---
+++ LeaveManagementWeb/Configutations/MapperConfig.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LeaveManagementWeb.Data;
+using LeaveManagementWeb.Models;
+
+namespace LeaveManagementWeb.Configutations
+{
+    public class MapperConfig:Profile
+    {
+        public MapperConfig() {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ LeaveManagementWeb/Models/LeaveTypeVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementWeb.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Display(Name ="Name Of Leave Type")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Default Number of Days")]
+        [Range(1,100,ErrorMessage ="Please enter a valid number")]
+        public int DefaultDays { get; set; }
+    }
+}
